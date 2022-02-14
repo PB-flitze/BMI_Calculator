f7d26ce3cd9208babc074d24c4695f383f92ad65
PB-flitze <breucker.pascal@web.de>
2022-02-14 19:00:05 +0100

    further implementations

--- src/bmi.ts
+++ src/bmi.ts
@@ -1,9 +1,11 @@
 
+
 function calculate_bmi(height:number, weight:number): number {
         height = height/100
     
         return weight/(height*height)
 }
 console.log("your BMI is " + calculate_bmi(180,80) );
+
 
 
---
+++ src/bmi_test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
+
+import { ibm } from "./bmi.ts"
+
+
+Deno.test("should yield correct values for mean/variance/stdev", async () => {
+
+    assertEquals(calculate_bmi(180,99))
